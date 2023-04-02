da022c97828a83238ace9a0886f00d4e508af9e7
Mehrael <mehraelashraf20@gmail.com>
2023-04-02 18:34:36 +0200

    first commit

---
+++ [TEMPLATE]/PresentBaskets/PresentBaskets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class PresentBaskets
+    {
+        #region YOUR CODE IS HERE
+        /// <summary>
+        /// fill the 2 baskets with the most expensive collection of fruits.
+        /// </summary>
+        /// <param name="W1">weight of 1st basket</param>
+        /// <param name="W2">weight of 2nd basket</param>
+        /// <param name="items">Pair of weight (Key) & cost (Value) of each item</param>
+        /// <returns>max total cost to fill two baskets</returns>
+        static public double PreparePresentBaskets(int W1, int W2, KeyValuePair<int, int>[] items)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
